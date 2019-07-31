5bf4af8643d2e4d9bf0e4b54e199b35814f20bc7
yoyomade <yoyomade@google.com>
2019-07-31 16:36:58 -0400

    updated files with new helper functions, updated appsscript with no V8

--- src/start.js
+++ src/start.js
@@ -12,7 +12,7 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
-// Global variables representing the placement of certain columns.
+// Global variables representing the index of certain columns.
 var COLUMN_NUMBER = {
   EMAIL: 3,
   HOURS_START: 4,
@@ -20,10 +20,17 @@ var COLUMN_NUMBER = {
   HOURLY_PAY: 9,
   CALC_PAY: 10,
   APPROVAL: 11,
+  NOTIFY: 12,
 };
 
+// TODO: Set these global variables accordingly!
+var APPROVED_EMAIL_SUBJECT = '';
+var REJECTED_EMAIL_SUBJECT = '';
+var APPROVED_EMAIL_MESSAGE = '';
+var REJECTED_EMAIL_MESSAGE = '';
+
 /** 
- * Creates the menu item "Timesheets" so user can run scripts on drop-down.
+ * Creates the menu item "Timesheets" for user to run scripts on drop-down.
  */
 function onOpen() {
   /**
@@ -33,36 +40,139 @@ function onOpen() {
 }
 
 /** 
- * Runs the "Column Setup" script. 
- * Adds "WEEKLY PAY" column with calculated
- * values using array formulas. 
- * Adds an "APPROVAL" column at the end of
- * the sheet, containing drop-down menus to either approve/disapprove employee
- * timesheets.  
+ * Adds "WEEKLY PAY" column with calculated values using array formulas. 
+ * Adds an "APPROVAL" column at the end of the sheet, containing 
+ * drop-down menus to either approve/disapprove employee timesheets.  
  * Adds a "NOTIFIED STATUS" column indicating whether or not an
  * employee has yet been e mailed.
-*/
+ */
 function columnSetup() {
+   /**
+   * TODO: Add code that calls helper functions to set up up three new columns,
+   * with necessary parameters.
+   */
+}
+
+/**
+ * Adds a CALCULATE PAY column and automatically calculates
+ * every employee's weekly pay.
+ *
+ * @param {Object} sheet Spreadsheet object of current sheet.
+ * @param {Integer} beginningRow Index of beginning row.
+ */
+function addCalculatePayColumn(sheet, beginningRow) {
   /**
-   * TODO: Add code that sets up three new columns.
-   * WEEKLY PAY needs to be filled with each employee's weekly pay value.
-   * APPROVAL needs to be made up of drop-down menus allowing for manager
+   * TODO: This column needs to be filled with each employee's weekly pay value.
+   */
+}
+
+/**
+ * Adds an APPROVAL column allowing managers to approve/
+ * disapprove of each employee's timesheet.
+ *
+ * @param {Object} sheet Spreadsheet object of current sheet.
+ * @param {Integer} beginningRow Index of beginning row.
+ * @param {Integer} numRows Number of rows currently in use.
+ */
+function addApprovalColumn(sheet, beginningRow, numRows) {
+  /** 
+   * TODO: This column needs to be made up of drop-down menus allowing for manager
    * approval/disapproval.
-   * NOTIFIED STATUS needs to be made up of drop-down menus signifying
+   */
+}
+
+/**
+ * Adds a NOTIFIED column allowing managers to see which employees
+ * have/have not yet been notified of their approval status.
+ *
+ * @param {Object} sheet Spreadsheet object of current sheet.
+ * @param {Integer} beginningRow Index of beginning row.
+ * @param {Integer} numRows Number of rows currently in use.
+ */
+function addNotifiedColumn(sheet, beginningRow, numRows) {
+  /** 
+   * TODO: This column needs to be made up of drop-down menus signifying
    * whether or not an employee has been notified. 
    */
 }
 
-/** 
- * Runs the script "Send Emails". Sends e mails to every employee notifying
- * them whether or not their timesheet has been approved.  Checks + updates
- * "NOTIFIED" status so employers do not send e mails more than once.
-*/
-function sendEmails() {
-  /**
-   * TODO: Add code that sends emails to all the employees notifying them of 
-   * their approval/disapproval status.
+/**
+ * Sends a notification email to employees, with the
+ * appropriate approval/disapproval subject and message.
+ *
+ * @param {String} email Employee's email.
+ * @param {String} emailSubject Appropriate email subject line regarding
+ * whether or not employee's timesheet has been approved.
+ * @param {String} emailMessage Appropriate email message body regarding
+ * whether or not employee's timesheet has been approved.
+ */
+function notify(email, emailSubject, emailMessage) {
+    /** 
+     * TODO: Use MailApp to send an automated email with the appropriate
+     * fields and parameters!
+     */
+}
+
+/**
+ * Sets the notification status to NOTIFIED for employees
+ * who have received a notification email.
+ *
+ * @param {Object} sheet Current Spreadsheet.
+ * @param {Object} notifiedValues Array of notified values.
+ * @param {Integer} i Current status in the for loop.
+ * @parma {Integer} beginningRow Row where iterations began.
+ */
+function updateNotifiedStatus(sheet, notifiedValues, i, beginningRow) {
+  /** 
+   * TODO: Set an employee's notified status to 'NOTIFIED', both in the
+   * notifiedValues array & reflected in the Sheet itself.
    */
 }
 
+/** 
+ * Checks the approval status of every employee, and calls helper functions
+ * to notify employees via email & update their notification status.
+ */
+function checkApprovedStatusToNotify() {
+  /**
+   * TODO: Insert code which 
+}
 
+/** 
+ * Set up the Timesheets Responses form, & link the form's trigger to 
+ * send manager an email when a new request is submitted.
+ */
+function setUpForm() {
+  /**
+   * TODO: Insert code which checks if a Form is already linked to this Sheet and,
+   * if not, creates a new form with the appropriate input values & names.
+   * Additionally, set up a form trigger which will call onFormSubmit every time
+   * a new response is submitted.
+   */
+}
+
+/**
+ * Handle new form submissions to trigger the workflow.
+ *
+ * @param {Object} event Form submit event
+ */
+function onFormSubmit(event) {
+  /**
+   * TODO: Insert code which inserts a new Form submission into the Sheet,
+   * where each field ends up in the appropriate column index.
+   */
+}
+
+/**
+ * Converts a form response to an object keyed by the item titles. Allows easier
+ * access to response values.
+ *
+ * @param {FormResponse} response
+ * @return {Object} Form values keyed by question title
+ */
+function getResponsesByName(response) {
+  /** 
+   * TODO: Insert the code which allows you to obtain a form's responses by
+   * their field names!
+   */
+}
